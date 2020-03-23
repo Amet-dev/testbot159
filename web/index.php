fd2684b621c174da7d972a5b85e19655738bd386
unknown <velilyaev1999@bk.ru>
2020-03-23 17:03:32 +0300

    UPD

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
 
 require('../vendor/autoload.php');
-
+get
 $app = new Silex\Application();
 $app['debug'] = true;
 
@@ -11,15 +11,36 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 ));
 
 // Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
+// $app->register(new Silex\Provider\TwigServiceProvider(), array(
+//     'twig.path' => __DIR__.'/views',
+// ));
 
 // Our web handlers
 
 $app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+  // $app['monolog']->addDebug('logging output.');
+  // return $app['twig']->render('index.twig');
+  return "hello world";
 });
+$app->post('/bot', function() use($app) {
+    $data = json_decode(file_get_contents('php://input'));
 
+    if(!$data)
+        return 'nioh';
+
+    if(!$data->secret !== getenv('VK_SECRET_TOKEN') && $data->type !== 'confirmation')
+        return 'nioh';
+
+    switch($data->type)
+    {
+        case 'confirmation':
+            return getenv('VK_SECRET_TOKEN')
+            break;
+        case 'massage_new':
+            // code...
+            break;
+    }
+
+    return "nioh";
+  });
 $app->run();
