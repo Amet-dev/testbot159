195ace09ba1200bd32f48a693f3a9b393c3fa5d7
Amet-Han <velilyaev1999@bk.ru>
2020-04-27 23:15:38 +0300

    1

--- web/index.php
+++ web/index.php
@@ -65,7 +65,7 @@ $app->post('/bot', function() use($app) {
               case 'Hello':
               case 'Hi':
             $user_id=$data->object->user_id;
-            $token=getenv('VK_TOKEN')
+            $token=getenv('VK_TOKEN');
             $user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&access_token={ $token }&v=5.69"));
             $qwerty='привет '. $user_info->response[0]->first_name;
                   $request_params['message'] = $qwerty;
